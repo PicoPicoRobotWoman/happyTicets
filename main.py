9f5610ca654a9193eebcbbf3a89dc8804ecea039
PicoPicoRobotWoman <i.ivanov@qvant.ru>
2023-11-10 10:45:46 +0300

    refactor: более лаконично записать

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
+from collections import defaultdict
+from itertools import product
 
 if __name__ == '__main__':
 
-    map = {}
+    map = defaultdict(int)
 
-    for n1 in range(0, 10):
-        for n2 in range(0, 10):
-            if n1 + n2 in map:
-                map[n1 + n2] += 1
-            else:
-                map[n1 + n2] = 1
+    for n1, n2 in product(range(10), repeat=2):
+        map[n1 + n2] += 1
 
-    res = {}
+    res = {n1: sum((map[n1 + n2] ** 2) - 1 for n2 in range(10)) for n1 in range(10)}
 
-    for n1 in range(0, 10):
-        res[n1] = 0
-        for n2 in range(0, 10):
-            res[n1] += (map[n1 + n2] ** 2) - 1
-
-    for k in res:
-        print(res[k])
+    for k, v in res.items():
+        print(f"рулон {k + 1} имеет: {v} счастливых билетов")
